cef17e9265809adb420f8fc84bf45916512595ea
Far-ch <148554202+Far-ch@users.noreply.github.com>
2024-01-25 23:13:47 +0330

    Which doctor.py

--- Which doctor.py
+++ Which doctor.py
@@ -1,11 +1,16 @@
+# Input two numbers
 a = int(input())
 b = int(input())
+# Calculate the XOR of the two numbers
 c = a ^ b
 d = str(bin(c))
+# Initialize a count to keep track of the set bits
 x = 0
+# Count the set bits in the XOR result
 for i in d:
     if i == '1':
         x += 1
+#print the number of bits to change
 print(x)
 
 
